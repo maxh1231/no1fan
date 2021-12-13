070e31bd53fb575f837919a4fe5088b1e46efbd4
Leithen <leithen113@gmail.com>
2021-12-12 18:03:23 -0700

    Created toggleable error message

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,26 @@
 const loginFormHandler = async (evt) => {
     evt.preventDefault();
+    const toggleErrMsg = () => {
+        document.querySelector('#error-message').classList.remove('display-none');
+        setTimeout(() => { document.querySelector('#error-message').classList.add('display-none') }, 2000);
+    };
     const email = document.querySelector('#email-input').value.trim();
     const password = document.querySelector('#password-input').value.trim();
     if (!email || !password) {
-        console.log('miss');
+        toggleErrMsg();
         return;
     }
     console.log('hit');
-    const response = await fetch('/user/', {
+    const response = await fetch('/api/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({ email: email, password: password })
     });
+    if (!response.ok) {
+        toggleErrMsg();
+        return;
+    }
+    console.log('logged in');
 };
 
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,30 @@
-const signupFormHandler = (evt) => {
+const signupFormHandler = async (evt) => {
     evt.preventDefault();
+    const toggleErrMsg = () => {
+        document.querySelector('#error-message').classList.remove('display-none');
+        setTimeout(() => { document.querySelector('#error-message').classList.add('display-none') }, 2000);
+    };
     const email = document.querySelector('#email-input').value.trim();
     const username = document.querySelector('#username-input').value.trim(); 
     const password = document.querySelector('#password-input').value.trim();
     const confirmPassword = document.querySelector('#confirm-password-input').value.trim();
     if (!email || !username || !password || !confirmPassword) {
-        console.log('miss');
+        toggleErrMsg();
+        return;
+    } else if (password !== confirmPassword) {
+        toggleErrMsg();
         return;
     }
-    console.log('hit');
+    const response = await fetch('/api/user/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ email: email, username: username, password: password })
+    });
+    if (!response.ok) {
+        toggleErrMsg();
+        return;
+    } 
+    console.log('user created');
 };
 
 document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
