9953d56396f495753502124a0705e07c6d50fa96
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-14 11:52:38 -0700

    fetch requests for artist, album and tracklist

--- public/js/artist-info.js
+++ public/js/artist-info.js
@@ -1,4 +1,6 @@
 const fetch = require('node-fetch');
+
+// artist info fetch- will be generated from a search 
 const deezerArtistURL = 'https://api.deezer.com/artist/27'
 
 let run = async () => {
@@ -16,4 +18,31 @@ run().then(res => {
     
 });
 
+//artist album fetch get artist id from previous fetch
+const deezerAlbumUrl = "https://api.deezer.com/artist/27/albums"
 
+let album = async () => {
+    const response = await fetch(deezerAlbumUrl);
+    if (!response.ok) {
+        console.log('error');
+    }
+    return (response.json());
+};
+
+album().then(res => {
+    console.log(res);
+});
+// get from album id from previous fetch 
+const deezerTrackListUrl = "https://api.deezer.com/album/302127"
+
+let trackList = async () => {
+    const response = await fetch(deezerTrackListUrl);
+    if (!response.ok) {
+        console.log('error');
+    }
+    return (response.json());
+};
+
+trackList().then(res => {
+    console.log(res);
+})
