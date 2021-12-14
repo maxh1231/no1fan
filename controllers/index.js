7383a05210e94c1f70a969561c9cde076e974db3
maxh1231 <maxhumpherys@gmail.com>
2021-12-14 15:20:40 -0700

    hardcoded HBS tracklist rendering

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,9 @@ router.use('/album', albumRoutes);
 
 // renders the artist info page with hardcoded info 
 router.use('/artistinfo', (req, res) => {
-    
-    
-    
+
+
+
     res.render('artist-info', {
         artistName: "Eminem",
         Album1: "Music to be Murdered By",
@@ -21,8 +21,38 @@ router.use('/artistinfo', (req, res) => {
         Album5: "Scary Movies",
         Album6: "Infinite",
         Shows: "Tomorrow"
-     
-        });
+
+    });
+});
+
+// renders the tracklist for album with hardcoded info
+router.use('/albuminfo', (req, res) => {
+
+
+
+    res.render('tracklist', {
+        AlbumName: "Curtains Up",
+        Track1: "White America",
+        Track2: "Business",
+        Track3: "Cleanin' Out My Closet",
+        Track4: "Square Dance",
+        Track5: "The Kiss",
+        Track6: "Soldier",
+        Track7: "Say Goodbye Hollywood",
+        Track8: "Drips",
+        Track9: "Without Me",
+        Track10: "Paul Rosenberg",
+        Track11: "Sing For The Moment",
+        Track12: "Superman",
+        Track13: "Hailie's Song",
+        Track14: "Steve Berman",
+        Track15: "When The Music Stops",
+        Track16: "Say What You Say",
+        Track17: "'Till I collapse",
+        Track18: "My Dad's Gone Crazy",
+        Track19: "Curtains Close"
+
+    });
 });
 
 router.use((req, res) => {
