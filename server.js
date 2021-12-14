bcd369bc0657b62d467d1fe9df22f9e840edf13f
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-14 11:08:57 -0700

    server rendering of hardcoded album and artist data

--- server.js
+++ server.js
@@ -35,6 +35,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
+
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
