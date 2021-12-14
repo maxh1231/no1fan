066de71ad52f7e6683fbf427194faf4ec1a6b294
maxh1231 <maxhumpherys@gmail.com>
2021-12-13 23:09:31 -0700

    C:/Program Files/Git/album route, tracklist.handlebars page rendering, deezer api fetch

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const albumRoutes = require('./album-routes')
 
 router.use('/api', apiRoutes);
+router.use('/album', albumRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
