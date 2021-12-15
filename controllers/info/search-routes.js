09456c451a5a2de32331106f7287374fedb6a1b8
Leithen <leithen113@gmail.com>
2021-12-15 16:41:04 -0700

    Created initial search fil structure

---
+++ controllers/info/search-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const fetch = require('node-fetch');
+
+router.get('/:input', async (req, res) => {
+    console.log(req.params.input);
+    const response = await fetch(`https://api.deezer.com/search/artist?q=${req.params.input}`);
+    res.render('search');
+})
+
+module.exports = router;
