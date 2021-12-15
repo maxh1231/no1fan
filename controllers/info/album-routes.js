b47ce3ed4db87435e0936196b042313333bcef55
maxh1231 <maxhumpherys@gmail.com>
2021-12-15 16:43:21 -0700

    handlebars rending 'data' confusion

--- controllers/info/album-routes.js
+++ controllers/info/album-routes.js
@@ -1,19 +1,35 @@
 const router = require('express').Router();
 const fetch = require('node-fetch');
-let albumID = '302127';
+let albumID = '51568522';
 
 
 // renders the tracklist for album with hardcoded info
 router.get('/', async (req, res) => {
-    const response = await fetch(`https://api.deezer.com/album/${albumID}/tracks`)
-    if (!response.ok) {
-        alert(response.statusText)
+    const response1 = await fetch(`https://api.deezer.com/album/${albumID}/tracks`)
+    if (!response1.ok) {
+        alert(response1.statusText)
     } else {
-        const data = await response.json();
-        console.log(data);
-        res.render('tracklist', data);
+        const response2 = await fetch(`https://api.deezer.com/album/${albumID}/`)
+        const data1 = await response1.json();
+        const data2 = await response2.json();
+        console.log(data1);
+        console.log(data2)
+        res.render('tracklist', data1);
+        res.render('tracklist', data2);
+
     }
 });
+
+// router.get('/', async (req, res) => {
+//     const response = await fetch(`https://api.deezer.com/album/${albumID}/`)
+//     if (!response.ok) {
+//         alert(response.statusText)
+//     } else {
+//         const data = await response.json();
+//         console.log(data);
+//         res.render('tracklist', data);
+//     }
+// });
 
 module.exports = router;
 
