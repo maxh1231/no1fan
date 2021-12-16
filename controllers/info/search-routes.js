a1e2a8d4fc94a2d9940e24eba77a823c73ce43cb
Leithen <leithen113@gmail.com>
2021-12-15 18:14:37 -0700

    Created basic search functionality

--- controllers/info/search-routes.js
+++ controllers/info/search-routes.js
@@ -3,8 +3,12 @@ const fetch = require('node-fetch');
 
 router.get('/:input', async (req, res) => {
     console.log(req.params.input);
-    const response = await fetch(`https://api.deezer.com/search/artist?q=${req.params.input}`);
-    res.render('search');
+    const response = await fetch(`https://api.deezer.com/search/artist?q=${req.params.input}&limit=5`);
+    if (!response.ok) {
+        alert(response.statusText);
+    }
+    const artists = await response.json()
+    res.render('search', {artists});
 })
 
 module.exports = router;
