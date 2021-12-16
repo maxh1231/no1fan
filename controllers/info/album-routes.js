92dbcb48384370b05d6e6c14ebe8ce80f1f43e1f
maxh1231 <maxhumpherys@gmail.com>
2021-12-15 17:16:26 -0700

    fetch both album data and tracklist of corresponding album

--- controllers/info/album-routes.js
+++ controllers/info/album-routes.js
@@ -5,19 +5,21 @@ let albumID = '51568522';
 
 // renders the tracklist for album with hardcoded info
 router.get('/', async (req, res) => {
-    const response1 = await fetch(`https://api.deezer.com/album/${albumID}/tracks`)
-    if (!response1.ok) {
-        alert(response1.statusText)
+    const response = await fetch(`https://api.deezer.com/album/${albumID}/`)
+    if (!response.ok) {
+        alert(response.statusText)
     } else {
-        const response2 = await fetch(`https://api.deezer.com/album/${albumID}/`)
-        const data1 = await response1.json();
-        const data2 = await response2.json();
-        console.log(data1);
-        console.log(data2)
-        res.render('tracklist', data1);
-        res.render('tracklist', data2);
+        const data = await response.json();
+        console.log(data);
+        let tracklist = JSON.stringify(data.tracks)
+        tracklist = JSON.parse(tracklist);
+        console.log(tracklist)
+        res.render('tracklist', { tracklist, data });
+
 
     }
+
+
 });
 
 // router.get('/', async (req, res) => {
