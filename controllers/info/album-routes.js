89434f7de5374dd93cafaad48785c1690420750a
maxh1231 <maxhumpherys@gmail.com>
2021-12-15 17:27:55 -0700

    removed backend parsing

--- controllers/info/album-routes.js
+++ controllers/info/album-routes.js
@@ -11,27 +11,9 @@ router.get('/', async (req, res) => {
     } else {
         const data = await response.json();
         console.log(data);
-        let tracklist = JSON.stringify(data.tracks)
-        tracklist = JSON.parse(tracklist);
-        console.log(tracklist)
-        res.render('tracklist', { tracklist, data });
-
-
+        res.render('tracklist', { data });
     }
-
-
 });
-
-// router.get('/', async (req, res) => {
-//     const response = await fetch(`https://api.deezer.com/album/${albumID}/`)
-//     if (!response.ok) {
-//         alert(response.statusText)
-//     } else {
-//         const data = await response.json();
-//         console.log(data);
-//         res.render('tracklist', data);
-//     }
-// });
 
 module.exports = router;
 
