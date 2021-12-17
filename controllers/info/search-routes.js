06ddb12ab373c4f12c320cecf5830d44a55c6f70
Leithen <leithen113@gmail.com>
2021-12-16 23:01:05 -0700

    Added more sections to search results page

--- controllers/info/search-routes.js
+++ controllers/info/search-routes.js
@@ -3,12 +3,17 @@ const fetch = require('node-fetch');
 
 router.get('/:input', async (req, res) => {
     console.log(req.params.input);
-    const response = await fetch(`https://api.deezer.com/search/artist?q=${req.params.input}&limit=5`);
-    if (!response.ok) {
-        alert(response.statusText);
+    const artistRes = await fetch(`https://api.deezer.com/search/artist?q=${req.params.input}&limit=4`);
+    if (!artistRes.ok) {
+        alert(artistRes.statusText);
     }
-    const artists = await response.json();
-    res.render('search', {artists});
+    const artists = await artistRes.json();
+    const trackRes = await fetch(`https://api.deezer.com/search/track?q=${req.params.input}&limit=5`);
+    if (!trackRes.ok) {
+        alert(trackRes.statusText);
+    }
+    const tracks = await trackRes.json();
+    res.render('search', {artists: artists, tracks: tracks});
 });
 
 module.exports = router;
