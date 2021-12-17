2bfa810923122ebd857148429dbc9917cb39865d
maxh1231 <maxhumpherys@gmail.com>
2021-12-17 16:45:58 -0700

    helper for release_date format

--- controllers/info/album-routes.js
+++ controllers/info/album-routes.js
@@ -10,8 +10,8 @@ router.get('/', async (req, res) => {
         alert(response.statusText)
     } else {
         const data = await response.json();
-        // console.log(data);
-        console.log(data.tracks)
+        console.log(data);
+        // console.log(data.tracks)
         res.render('tracklist', { data });
     }
 });
