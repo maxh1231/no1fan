121645d1da9a987314189fa4571479a2c710177f
Ben H <deepfreespace@gmail.com>
2021-12-17 21:45:23 -0700

    Router/js scaffolding

--- controllers/info/index.js
+++ controllers/info/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const artistRoutes = require('./artist-routes.js');
 const albumRoutes = require('./album-routes');
 const searchRoutes = require('./search-routes');
+const setlistRoutes = require('./setlist-routes');
 
 router.use('/artist', artistRoutes);
 router.use('/album', albumRoutes);
 router.use('/search', searchRoutes);
+router.use('/setlist', setlistRoutes);
 
 module.exports = router;
---
+++ controllers/info/setlist-routes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const fetch = require('node-fetch');
+
+module.exports = router;
