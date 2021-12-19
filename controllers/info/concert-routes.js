184525265866b871a4d8e7c55be7fffca2b1e1f5
Ben H <deepfreespace@gmail.com>
2021-12-19 16:48:24 -0700

    Upcoming concerts using geolocation route

--- controllers/info/concert-routes.js
+++ controllers/info/concert-routes.js
@@ -5,10 +5,20 @@ const fetch = require('node-fetch');
 // Endpoint is ./info/concerts
 
 // Get upcoming concerts for one artist
-router.get('/:input', async (req, res) => {
+router.get('/artist/:input', async (req, res) => {
     const concerts = await (
         await fetch(
             `https://api.seatgeek.com/2/events?performers.slug=${req.params.input}&client_id=${process.env.SEATGEEK_CLIENT_ID}&client_secret=${process.env.SEATGEEK_SECRET}`
+        )
+    ).json();
+    res.send(concerts);
+});
+
+// Get upcoming concerts using user's location
+router.get('/', async (req, res) => {
+    const concerts = await (
+        await fetch(
+            `https://api.seatgeek.com/2/events?taxonomies.name=concert&geoip=true&client_id=${process.env.SEATGEEK_CLIENT_ID}&client_secret=${process.env.SEATGEEK_SECRET}`
         )
     ).json();
     res.send(concerts);
