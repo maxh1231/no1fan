44295ca35978754d87b15b49453b7202c5906c67
Leithen <leithen113@gmail.com>
2021-12-19 18:38:23 -0700

    added basic geo location to homepage for later use

--- public/js/home.js
+++ public/js/home.js
@@ -4,7 +4,6 @@ const search = (evt) => {
     document.location.assign(`/info/search/${input}`)
 }
 
-
 const getInfo = (evt) => {
     evt.preventDefault();
     let current = '';
@@ -18,3 +17,18 @@ const getInfo = (evt) => {
 
 document.querySelector('#top-artist-container').addEventListener('click', getInfo);
 document.querySelector('#home-search-form').addEventListener('submit', search);
+
+var x = document.getElementById("location");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+getLocation();
