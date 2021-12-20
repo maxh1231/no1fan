8090515a86acf2a9cb84cea5099bea4c8f6fdd75
Ben H <deepfreespace@gmail.com>
2021-12-20 12:54:26 -0700

    Setlist and concert routes done(?), skeletons for handlebars elements

--- controllers/info/setlist-routes.js
+++ controllers/info/setlist-routes.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 // Endpoint is ./info/setlists/
 
-// Get most recent setlists for one artist
+// Get most recent setlists/shows for one artist
 router.get('/artist/:input', async (req, res) => {
     // Get artist MBID to feed into Setlist API
     const artistMBID = (
@@ -21,7 +21,7 @@ router.get('/artist/:input', async (req, res) => {
         ).json()
     ).artist[0].mbid;
     // Use MBID to get recent setlists
-    const artistSetlist = await (
+    const artistSetlists = await (
         await fetch(
             `https://api.setlist.fm/rest/1.0/artist/${artistMBID}/setlists?p=1`,
             {
@@ -33,7 +33,41 @@ router.get('/artist/:input', async (req, res) => {
         )
     ).json();
     // Send setlist body back
-    res.send(artistSetlist);
+    res.send(artistSetlists);
+});
+
+// Get most recent setlists/shows for one venue
+router.get('/venue/:input', async (req, res) => {
+    // https://api.setlist.fm/rest/1.0/search/venues?p=1&cityName=salt%20lake%20city&name=the%20complex
+    // How to pull two different sets of params?
+    // Get venue ID to feed into Setlist API
+    const venueID = (
+        await (
+            await fetch(
+                `https://api.setlist.fm/rest/1.0/search/venues?name=${req.params.input}&p=1`,
+                {
+                    headers: {
+                        Accept: 'application/json',
+                        'x-api-key': process.env.SETLIST_API_KEY,
+                    },
+                }
+            )
+        ).json()
+    ).venue[0].id;
+    // Use venue ID to get recent setlists
+    const venueSetlists = await (
+        await fetch(
+            `https://api.setlist.fm/rest/1.0/venue/${venueID}/setlists?p=1`,
+            {
+                headers: {
+                    Accept: 'application/json',
+                    'x-api-key': process.env.SETLIST_API_KEY,
+                },
+            }
+        )
+    ).json();
+    // Send setlist body back
+    res.send(venueSetlists);
 });
 
 // Exports
