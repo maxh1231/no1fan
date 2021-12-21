60655da0f5317e9018838000cb8d66a75b071b8d
maxh1231 <maxhumpherys@gmail.com>
2021-12-20 20:51:25 -0700

    post and delete request

--- controllers/api/artist-routes.js
+++ controllers/api/artist-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
     ArtistFavorites.findOne({
 
         where: {
-            id: req.session.id
+            user_id: req.session.id
         },
         attributes: [
             'id',
@@ -40,5 +40,41 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/', (req, res) => {
+    ArtistFavorites.create({
+        artist_id: req.body.artist_id,
+        artist_name: req.body.artist_name,
+        user_id: req.session.id
+
+    })
+
+        .then(favorites => res.json(favorites))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.delete('/:id', (req, res) => {
+    ArtistFavorites.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(favorites => {
+            if (favorites) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(favorites);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
 
 module.exports = router;
