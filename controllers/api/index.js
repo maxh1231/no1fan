52c93248f0613ac99299b05ab0983cdaea723ac2
maxh1231 <maxhumpherys@gmail.com>
2021-12-20 21:23:28 -0700

    forgot to included album routes in api/index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const ArtistFavorites = require('./artist-routes');
+const artistFavorites = require('./artist-routes');
+const albumFavorites = require('./album-routes');
 
 
 router.use('/user', userRoutes);
-router.use('/artistfavorites', ArtistFavorites);
+router.use('/artistfavorites', artistFavorites);
+router.use('/albumfavorites', albumFavorites);
 
 
 router.use((req, res) => {
