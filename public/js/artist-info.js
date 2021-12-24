d6a450c3214e74639f31d6fe9eae753490f67435
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-23 18:02:28 -0700

    working on styling, also changed recommended artistst to more artists

--- public/js/artist-info.js
+++ public/js/artist-info.js
@@ -28,28 +28,28 @@ const getRecommended = (evt) => {
 // code for the carousel display
 document.querySelectorAll(".carousel").forEach((carousel) => {
     // sees how many items we have 
-    const items = carousel.querySelectorAll(".carousel__item");
+    const items = carousel.querySelectorAll(".carousel-display-item");
     // creates a button for each item - should be able to generate off of our artist card 
     const buttonsHtml = Array.from(items, () => {
-      return `<span class="carousel__button"></span>`;
+      return `<span class="carousel-button"></span>`;
     });
     // insert buttons into html before end 
     carousel.insertAdjacentHTML('beforeend', `
-    <div class="carousel__nav">
+    <div class="carousel-navigation">
     ${ buttonsHtml.join("") }
     </div>
     `);
     // select each button element 
-const buttons = carousel.querySelectorAll('.carousel__button');
+const buttons = carousel.querySelectorAll('.carousel-button');
 // add event listener for each button 
 buttons.forEach((button, i) => {
 button.addEventListener("click", () => {
     // un-select all the items
-    items.forEach(item => item.classList.remove("carousel__item--selected"));
-    buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+    items.forEach(item => item.classList.remove("carousel-display-item-selected"));
+    buttons.forEach(button => button.classList.remove("carousel-button-selected"));
 
-    items[i].classList.add('carousel__item--selected');
-    button.classList.add("carousel__button--selected");
+    items[i].classList.add('carousel-display-item-selected');
+    button.classList.add("carousel-button-selected");
 });
 });
 
@@ -58,8 +58,8 @@ button.addEventListener("click", () => {
 
 
 // select the first item on the page on load 
-items[0].classList.add('carousel__item--selected');
-    buttons[0].classList.add("carousel__button--selected");
+items[0].classList.add('carousel-display-item-selected');
+    buttons[0].classList.add("carousel-button-selected");
 });
 
 
