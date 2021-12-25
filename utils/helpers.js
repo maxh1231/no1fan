d075df8a0386f3564686e6fc003bc5fa157e0033
Thorulfr <44299035+Thorulfr@users.noreply.github.com>
2021-12-24 18:20:35 -0700

    URL helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,12 @@
 const moment = require('moment');
 
 module.exports = {
-    plus_one: index => {
-        return index + 1
+    plus_one: (index) => {
+        return index + 1;
     },
-    format_time: time => {
+    format_time: (time) => {
         var min = Math.floor(time / 60);
-        var sec = time - (min * 60);
+        var sec = time - min * 60;
         if (sec.toString().length < 2) {
             return `${min}:0${sec}`;
         } else {
@@ -14,22 +14,24 @@ module.exports = {
         }
     },
 
-    format_duration: time => {
+    format_duration: (time) => {
         var min = Math.floor(time / 60);
         var hour = Math.floor(min / 60);
         if (time > 3600) {
-            min = Math.floor(min % 60)
-            return `${hour} h ${min} mins`
+            min = Math.floor(min % 60);
+            return `${hour} h ${min} mins`;
         } else {
-            return `${min} mins`
+            return `${min} mins`;
         }
-
     },
-    format_date: date => {
+    format_date: (date) => {
         if (date.length === 10) {
-            return moment(date).format("MM-DD-YYYY")
+            return moment(date).format('MM-DD-YYYY');
         }
-        return moment(date).format("MM-DD-YYYY h:mma");
+        return moment(date).format('MM-DD-YYYY h:mma');
     },
-
-}
+    format_venue_name: (name) => {
+        const noSpacesName = name.replace(' ', '%20');
+        return noSpacesName;
+    },
+};
