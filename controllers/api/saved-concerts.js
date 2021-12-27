d76b45ae2665966bf68111c8d76b38ffa970e105
Ben H <deepfreespace@gmail.com>
2021-12-27 16:43:10 -0700

    Get all saved concerts

--- controllers/api/saved-concerts.js
+++ controllers/api/saved-concerts.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
-const { AlbumFavorites, ArtistFavorites } = require('../../models');
+const { SavedConcerts } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // Get all saved concerts
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    SavedConcerts.findAll({
+        where: {
+            user_id: req.session.user_id,
+        },
+        attributes: [
+            'id',
+            'artist_id',
+            'artist_name',
+            'venue_name',
+            'date',
+            'user_id',
+        ],
+    })
+        .then((concerts) => res.json(concerts))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Add a saved concert
