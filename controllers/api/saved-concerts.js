5c4c7e2608d76c1c9cfa3c3252be5b8213c21556
Ben H <deepfreespace@gmail.com>
2021-12-27 16:50:07 -0700

    Save concert route

--- controllers/api/saved-concerts.js
+++ controllers/api/saved-concerts.js
@@ -25,3 +25,17 @@ router.get('/', (req, res) => {
 });
 
 // Add a saved concert
+router.post('/', (req, res) => {
+    SavedConcerts.create({
+        artist_id: req.body.artist_id,
+        artist_name: req.body.artist_name,
+        venue_name: req.body.venue_name,
+        date: req.body.date,
+        user_id: req.session.user_id,
+    })
+        .then((concerts) => res.json(concerts))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
