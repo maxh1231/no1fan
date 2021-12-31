7f00c641e8e1f9e8fdd514fbd103e370f4216962
Leithen <leithen113@gmail.com>
2021-12-31 07:00:04 -0700

    Got user info sent to dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fetch = require('node-fetch');
+const { User, ArtistFavorites, AlbumFavorites, SavedConcerts  } = require('../models');
 
 // get home
 router.get('/', async (req, res) => {
@@ -43,8 +44,29 @@ router.get('/login', (req, res) => {
 });
 
 // get dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard', { loggedIn: req.session.loggedIn });
+router.get('/dashboard', async (req, res) => {
+    const response = await User.findOne({
+        where: { id: req.session.user_id },
+        attributes: { exclude: ['password'] },
+        include: [
+            {
+                model: ArtistFavorites,
+                attributes: ['id', 'artist_id', 'artist_name', 'user_id']
+            },
+            {
+                model: AlbumFavorites,
+                attributes: ['id', 'album_id', 'album_name', 'user_id']
+            },
+            {
+                model: SavedConcerts,
+                attributes: ['id', 'artist_name', 'venue_name', 'date', 'setlist_url', 'user_id']
+            }
+        ]
+    });
+    // console.log(response);
+    const data = response.dataValues
+    console.log(data)
+    res.render('dashboard', { data, loggedIn: req.session.loggedIn });
 });
 
 module.exports = router;
