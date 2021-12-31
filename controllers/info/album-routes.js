cee4c6de8f69a055f861d7e10158b28cc0349c99
Leithen <leithen113@gmail.com>
2021-12-31 16:19:37 -0700

    Added iFrame to venue page

--- controllers/info/album-routes.js
+++ controllers/info/album-routes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const fetch = require('node-fetch');
 
-
-
-// renders the tracklist for album with hardcoded info
+// renders the tracklist for album
 router.get('/:id', async (req, res) => {
     let albumID = req.params.id;
     const response = await fetch(`https://api.deezer.com/album/${albumID}/`)
@@ -17,4 +15,3 @@ router.get('/:id', async (req, res) => {
 });
 
 module.exports = router;
-
