d532b4f082168ec2fcc88a907914a1b2cdf95461
Ben H <deepfreespace@gmail.com>
2022-01-01 18:36:30 -0700

    small dashboard changes

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,10 +1,18 @@
-const home = () => {document.location.assign('/')};
+const home = () => {
+    document.location.assign('/');
+};
 
-const login = () => {document.location.assign('/login')};
+const login = () => {
+    document.location.assign('/login');
+};
 
-const signup = () => {document.location.assign('/signup')};
+const signup = () => {
+    document.location.assign('/signup');
+};
 
-const dashboard = () => {document.location.replace('/dashboard')};
+const dashboard = () => {
+    document.location.replace('/dashboard');
+};
 
 const search = (evt) => {
     evt.preventDefault();
@@ -17,7 +25,7 @@ const logout = async () => {
         method: 'POST',
     });
     if (!response.ok) {
-        alert(response.statusText); // TODO: Remove this alert before pushing!!
+        alert(response.statusText);
         return;
     }
     document.location.assign('/');
@@ -27,13 +35,21 @@ document.querySelector('#home-btn').addEventListener('click', home);
 document.querySelector('#home-search-form').addEventListener('submit', search);
 
 const loginBtn = document.querySelector('#login-btn');
-if (loginBtn) {loginBtn.addEventListener('click', login);}
+if (loginBtn) {
+    loginBtn.addEventListener('click', login);
+}
 
 const signupBtn = document.querySelector('#signup-btn');
-if (signupBtn) {signupBtn.addEventListener('click', signup);}
+if (signupBtn) {
+    signupBtn.addEventListener('click', signup);
+}
 
 const dashBtn = document.querySelector('#dashboard-btn');
-if (dashBtn) {dashBtn.addEventListener('click', dashboard);}
+if (dashBtn) {
+    dashBtn.addEventListener('click', dashboard);
+}
 
 const logoutBtn = document.querySelector('#logout-btn');
-if (logoutBtn) {logoutBtn.addEventListener('click', logout);}
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', logout);
+}
